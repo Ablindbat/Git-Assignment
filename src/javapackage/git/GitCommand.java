67a2c8730651d3f47dbd9e17948839878ecc8fc9
ablindbat <rajibmahato885@gmail.com>
2022-12-23 12:39:23 +0530

    First commit

---
+++ src/javapackage/git/GitCommand.java
@@ -0,0 +1,29 @@
+package javapackage.git;
+
+class Action{
+	double a = 45,b = 5,sum;
+	double value=0;
+	public void addTwoNumber() {
+		sum = a+b;
+		System.out.println("Sum: "+sum);
+	}
+	public void minusTwoNumber() {
+		value = a - b;
+		System.out.println("Subtract: "+value);
+	}
+	public void multipleTwoNumber() {
+		value = a * b;
+		System.out.println("Multiply value: "+value);
+	}
+}
+public class GitCommand {
+
+	public static void main(String[] args) {
+		Action action = new Action();
+		action.addTwoNumber();
+		action.minusTwoNumber();
+		action.multipleTwoNumber();
+
+	}
+
+}
